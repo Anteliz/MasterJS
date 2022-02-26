89292566c045a1d945bd27e35fc829a2afe12d13
Adolfo Anteliz <adolfo_anteliz@outlook.com>
2022-02-25 22:09:39 -0600

    Segunda parte del node js

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -1,6 +1,8 @@
 'use strinct'
 
 var validator = require('validator');
+const { findByIdAndUpdate } = require('../models/article');
+const article = require('../models/article');
 var Article = require('../models/article')
 
 var controller = {
@@ -94,6 +96,124 @@ var controller = {
                 status: 'Success',
                 articles
             });
+        });
+    },
+
+    getArticle: (req, res) => {
+        // Recoger la URL
+        var articleId = req.params.id;
+
+        // Comprobar que existe
+        if(!articleId || articleId == null){
+            return res.status(404).send({
+                status: 'Error',
+                message: 'No existe el Artículo'
+            });
+        }
+
+        // Buscar el artículo
+        return Article.findById(articleId, (err, article)=>{
+            if(err){
+                return res.status(500).send({
+                    status: 'Error',
+                    message: 'Error al devolver los datos'
+                });
+            }
+
+            if(!article){
+                return res.status(404).send({
+                    status: 'Error',
+                    message: 'No existe el Artículo'
+                });
+            }
+
+            //Devolver el Json
+            return res.status(200).send({
+                status: 'Success',
+                article
+            });
+        });
+    },
+
+    update: (req, res) =>{
+        // Recoger el id  del artículo por la URL
+        var articleId = req.params.id;
+
+        // Recoger los datos que llegan por PUT
+        var params = req.body;
+
+        // Validar datos
+        try{
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+
+        }
+        catch(err){
+            return res.status(404).send({
+                status: 'Error',
+                message: 'Falta datos por enviar'
+            });
+        }
+
+        if(validate_title && validate_content){
+            // Find and Update
+            return Article.findByIdAndUpdate({_id: articleId}, params, {new : true}, (err, articleUpdated) => {
+                if(err){
+                    return res.status(500).send({
+                        status: 'Error',
+                        message: 'Error al actualizar'
+                    });
+                }
+                if(!articleUpdated){
+                    return res.status(500).send({
+                        status: 'Error',
+                        message: 'No existe el artículo'
+                    });
+                }
+
+                //Devolver respuesta
+                return res.status(200).send({
+                    status: 'success',
+                    articleUpdated
+                });
+            });
+        }
+        else{
+            return res.status(500).send({
+                status: 'Error',
+                message: 'La validación no es correcta'
+            });
+        }
+    }, 
+
+    delete: (req, res) =>{
+        //Recoger el id de la url
+        var articleId = req.params.id;
+
+        // Find and Delete
+        return Article.findByIdAndDelete({_id:articleId}, (err, articleRemoved)=>{
+            if(err){
+                return res.status(500).send({
+                    status: 'Error',
+                    message: 'Error al borrar'
+                });
+            }
+
+            if(!articleRemoved){
+                return res.status(404).send({
+                    status: 'Error',
+                    message: 'No se a borrado el artículo, posiblemente no existe'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'Success',
+                article: articleRemoved
+            });
+        });
+        return res.status(500).send({
+            status: 'Error',
+            message: 'test de delete'
         });
     }
 }
